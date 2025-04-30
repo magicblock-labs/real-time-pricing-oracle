f5df43feeacfc4dd9fe1273b8970f7e99fbf8f8c
Luca <luca@fedora.fritz.box>
2025-04-30 19:49:38 +0200

    Add supervisor conf

---
+++ supervisor_generate_config.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+SYMBOLS_URL="https://history.pyth-lazer.dourolabs.app/history/v1/symbols"
+TMP_FILE=$(mktemp)
+
+# Download and save to temporary file
+curl -s "$SYMBOLS_URL" | jq -c '.[:40]' > "$TMP_FILE"
+
+# Count symbols
+SYMBOLS_COUNT=$(jq -r 'length' "$TMP_FILE")
+
+mkdir -p /etc/supervisor/conf.d
+: > /etc/supervisor/conf.d/real_time_pricing_oracle.conf
+
+COUNTER=1
+i=0
+while [ "$i" -lt "$SYMBOLS_COUNT" ]; do
+    GROUP=$(jq -c ".[$i:$((i+4))]" "$TMP_FILE")
+
+    CHANNEL=$(echo "$GROUP" | jq -r '.[0].min_channel')
+    PRICE_FEEDS=$(echo "$GROUP" | jq -r '[.[].name] | join(",")')
+
+    cat >> /etc/supervisor/conf.d/real_time_pricing_oracle.conf <<EOL
+[program:symbol-fetcher-$COUNTER]
+command=env ORACLE_WS_URL="wss://pyth-lazer.dourolabs.app/v1/stream" \
+        ORACLE_AUTH_HEADER="Bearer qyfk9GJqmyYxjfW8ANUplSsnMnMa64Ai0XK-mgbl" \
+        SOLANA_CLUSTER="http://localhost:8899" \
+        ORACLE_PRICE_FEEDS="$PRICE_FEEDS" \
+        ORACLE_CHANNEL="$CHANNEL" \
+        ephemeral-pricing-oracle
+autostart=true
+autorestart=true
+stdout_logfile=/dev/stdout
+stderr_logfile=/dev/stderr
+stdout_logfile_maxbytes=0
+stderr_logfile_maxbytes=0
+
+EOL
+
+    COUNTER=$((COUNTER+1))
+    i=$((i+4))
+done
+
+rm "$TMP_FILE"
+echo "Generated $((COUNTER-1)) Oracle supervisor configurations"
